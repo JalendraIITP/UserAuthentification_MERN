c0285f069938d35eac205d847a5cba0c9810891b
Jalendra Gouda <101317808+JalendraIITP@users.noreply.github.com>
2024-01-22 15:51:18 +0530

    Update Navbar.js

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
     };
 
     const sendLogout = async () => {
-        const res = await axios.post('http://localhost:4000/logout', null, {
+        const res = await axios.post('https://backend-gaqp.onrender.com/logout', null, {
             withCredentials: true
         });
         if (res.status === 200) {
