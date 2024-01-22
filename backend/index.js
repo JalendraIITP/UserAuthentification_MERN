49474e4e0c9e22ecc026e15d02452a978e34e950
Jalendra Gouda <101317808+JalendraIITP@users.noreply.github.com>
2024-01-22 15:59:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["https://imagegallery-uln5.onrender.com"],
     method:["POST","GET"],
     credentials:true
 }));
