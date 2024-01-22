64a5e81c45c2581ac5a49e5d3dc1b75dd9e67712
Jalendra Gouda <101317808+JalendraIITP@users.noreply.github.com>
2024-01-22 15:40:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors({
-    origin:"https://deploy-mern-lwhq.versel.app"],
+    origin:"*"],
     method:["POST","GET"],
     credentials:true
 }));
