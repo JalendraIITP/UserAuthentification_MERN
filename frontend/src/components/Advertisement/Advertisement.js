5e22a979a95c6201eaad84683dc40390e0d4e356
Jalendra Gouda <101317808+JalendraIITP@users.noreply.github.com>
2024-01-31 22:09:47 +0530

    Update Advertisement.js

--- frontend/src/components/Advertisement/Advertisement.js
+++ frontend/src/components/Advertisement/Advertisement.js
@@ -12,10 +12,8 @@ const Advertisement = () => {
     return (
         <>
             <div className='adv1'>
-                <p>Welcome to Image Gallery</p>
-                {user && <Link to={`/user/${user.name}`}><button><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-                </svg></button></Link>}
+                {user && <p>Welcome to Image Gallery</p>}
+                {!user && <Link to={`/user/${user.name}`}><p>{user.name}</p><Link/>}
             </div>
             <div className="gallery">
                 <img src={im1} alt="Two hands creating a heart and showing the sun" />
