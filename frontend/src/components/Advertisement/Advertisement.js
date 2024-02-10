8a7e4e78c8569913c8b4ca98258936cc1263793f
Jalendra Gouda <101317808+JalendraIITP@users.noreply.github.com>
2024-02-11 01:16:19 +0530

    Update Advertisement.js

--- frontend/src/components/Advertisement/Advertisement.js
+++ frontend/src/components/Advertisement/Advertisement.js
@@ -1,1 +1,25 @@
+import React from 'react'
+import './Advertisement.css'
+import ImageSlider from './Slider'
+
+const Advertisement = () => {
+    const containerStyles = {
+        width: "1520px",
+        height: "580px",
+        margin: "0 auto",
+    };
+
+    return (
+        <>
+            <div className='adv2'>
+                <p>Welcome to Image Gallery</p>
+            </div>
+            <div style={containerStyles}>
+                <ImageSlider />
+            </div>
+        </>
+    );
+}
+
+export default Advertisement
 
